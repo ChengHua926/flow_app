1931a57acfde6480a129f513a55d9703fae4bb3a
ChengHua926 <25chenghua@gmail.com>
2023-10-11 21:33:03 -0400

    updated progress bar and number of students

--- lib/screen/code_page.dart
+++ lib/screen/code_page.dart
@@ -10,14 +10,15 @@ class CodePage extends StatefulWidget {
 }
 
 class _CodePageState extends State<CodePage> {
-  bool isUrlSet = false;
-
   final AudioPlayer jplayer = AudioPlayer();
-  bool isPlaying = false;
+  final ValueNotifier<bool> isPlaying = ValueNotifier<bool>(false);
+  bool isUrlSet = false;
 
   @override
   void dispose() {
+    jplayer.stop(); // Stop the audio playback
     jplayer.dispose();
+    isPlaying.dispose();
     super.dispose();
   }
 
@@ -38,7 +39,7 @@ class _CodePageState extends State<CodePage> {
         child: Column(
           children: [
             Padding(
-              padding: const EdgeInsets.only(top: 120.0, bottom: 30.0),
+              padding: const EdgeInsets.only(top: 120.0, bottom: 10.0),
               child: Text(
                 "Game Code: $gameCode",
                 style: const TextStyle(
@@ -64,59 +65,166 @@ class _CodePageState extends State<CodePage> {
                   }
                   final players = snapshot.data!.docs;
 
-                  return ListView.builder(
-                    padding: const EdgeInsets.symmetric(
-                        horizontal: 20.0, vertical: 10.0),
-                    itemCount: players.length,
-                    itemBuilder: (context, index) {
-                      final playerName = players[index].id;
-                      return Padding(
-                        padding: const EdgeInsets.symmetric(vertical: 8.0),
-                        child: Container(
-                          padding: const EdgeInsets.symmetric(
-                              horizontal: 20, vertical: 10),
-                          decoration: BoxDecoration(
-                            color: Colors.white.withOpacity(0.7),
-                            borderRadius: BorderRadius.circular(10),
-                            boxShadow: [
-                              BoxShadow(
-                                color: Colors.black.withOpacity(0.1),
-                                spreadRadius: 1,
-                                blurRadius: 3,
-                                offset: const Offset(0, 2),
-                              ),
-                            ],
-                          ),
-                          child: Text(
-                            playerName,
-                            style: const TextStyle(
-                              fontSize: 20,
-                              fontWeight: FontWeight.bold,
-                              color: Colors.black,
-                            ),
+                  return Column(
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.symmetric(vertical: 10.0),
+                        child: Text(
+                          '${players.length} students have joined',
+                          style: TextStyle(
+                            fontSize: 16,
+                            color: Colors.black.withOpacity(0.7),
                           ),
                         ),
-                      );
-                    },
+                      ),
+                      Expanded(
+                        child: ListView.builder(
+                          padding: const EdgeInsets.symmetric(
+                              horizontal: 20.0, vertical: 10.0),
+                          itemCount: players.length,
+                          itemBuilder: (context, index) {
+                            final playerName = players[index].id;
+                            return Padding(
+                              padding:
+                                  const EdgeInsets.symmetric(vertical: 8.0),
+                              child: Container(
+                                padding: const EdgeInsets.symmetric(
+                                    horizontal: 20, vertical: 10),
+                                decoration: BoxDecoration(
+                                  color: Colors.white.withOpacity(0.7),
+                                  borderRadius: BorderRadius.circular(10),
+                                  boxShadow: [
+                                    BoxShadow(
+                                      color: Colors.black.withOpacity(0.1),
+                                      spreadRadius: 1,
+                                      blurRadius: 3,
+                                      offset: const Offset(0, 2),
+                                    ),
+                                  ],
+                                ),
+                                child: Text(
+                                  playerName,
+                                  style: const TextStyle(
+                                    fontSize: 20,
+                                    fontWeight: FontWeight.bold,
+                                    color: Colors.black,
+                                  ),
+                                ),
+                              ),
+                            );
+                          },
+                        ),
+                      ),
+                    ],
                   );
                 },
-                // ... (rest of your StreamBuilder code)
               ),
             ),
             StreamBuilder<Duration>(
-              stream: jplayer.durationStream
-                  .map((event) => event ?? Duration.zero), // Handle null values
-              builder: (context, durationSnapshot) {
-                final duration = durationSnapshot.data ?? Duration.zero;
+              stream: jplayer.positionStream,
+              builder: (context, positionSnapshot) {
+                final position = positionSnapshot.data ?? Duration.zero;
                 return Padding(
                   padding: const EdgeInsets.symmetric(horizontal: 20.0),
                   child: Row(
-                    mainAxisAlignment: MainAxisAlignment.start,
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
                     children: [
                       Text(
-                        "${duration.inMinutes}:${duration.inSeconds.remainder(60).toString().padLeft(2, '0')}", // Formatting the duration to MM:SS format
+                        "${position.inMinutes}:${position.inSeconds.remainder(60).toString().padLeft(2, '0')}", // Formatting the position to MM:SS format
                         style: const TextStyle(
                           fontSize: 16,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.black,
+                        ),
+                      ),
+                      Expanded(
+                        child: Slider(
+                          onChanged: (newValue) {
+                            jplayer
+                                .seek(Duration(milliseconds: newValue.toInt()));
+                          },
+                          value: position.inMilliseconds.toDouble(),
+                          max: jplayer.duration?.inMilliseconds.toDouble() ??
+                              100.0,
+                        ),
+                      ),
+                      StreamBuilder<Duration>(
+                        stream: jplayer.durationStream
+                            .map((event) => event ?? Duration.zero),
+                        builder: (context, durationSnapshot) {
+                          final duration =
+                              durationSnapshot.data ?? Duration.zero;
+                          return Text(
+                            "${duration.inMinutes}:${duration.inSeconds.remainder(60).toString().padLeft(2, '0')}", // Formatting the duration to MM:SS format
+                            style: const TextStyle(
+                              fontSize: 16,
+                              fontWeight: FontWeight.bold,
+                              color: Colors.black,
+                            ),
+                          );
+                        },
+                      ),
+                    ],
+                  ),
+                );
+              },
+            ),
+            // StreamBuilder<Duration>(
+            //   stream: jplayer.positionStream,
+            //   builder: (context, snapshot) {
+            //     final position = snapshot.data ?? Duration.zero;
+            //     return Slider(
+            //       onChanged: (newValue) {
+            //         jplayer.seek(Duration(milliseconds: newValue.toInt()));
+            //       },
+            //       value: position.inMilliseconds.toDouble(),
+            //       max: jplayer.duration?.inMilliseconds.toDouble() ?? 100.0,
+            //     );
+            //   },
+            // ),
+            ValueListenableBuilder<bool>(
+              valueListenable: isPlaying,
+              builder: (context, isPlayingValue, child) {
+                return Padding(
+                  padding: const EdgeInsets.all(30.0),
+                  child: Column(
+                    children: [
+                      FloatingActionButton(
+                        onPressed: () async {
+                          if (!isPlayingValue) {
+                            if (!isUrlSet) {
+                              DocumentSnapshot gameSession =
+                                  await FirebaseFirestore.instance
+                                      .collection('game sessions')
+                                      .doc(gameCode)
+                                      .get();
+
+                              String? audioUrl = (gameSession.data()
+                                  as Map<String, dynamic>)['audio url'];
+
+                              if (audioUrl != null && audioUrl.isNotEmpty) {
+                                await jplayer.setUrl(audioUrl);
+                                isUrlSet = true;
+                              } else {
+                                print("Audio URL not found or is empty");
+                                return;
+                              }
+                            }
+                            jplayer.play();
+                          } else {
+                            jplayer.pause();
+                          }
+                          isPlaying.value = !isPlayingValue;
+                        },
+                        backgroundColor: Colors.black,
+                        child: Icon(
+                            isPlayingValue ? Icons.pause : Icons.play_arrow),
+                      ),
+                      const SizedBox(height: 10),
+                      const Text(
+                        "Play",
+                        style: TextStyle(
+                          fontSize: 18,
                           fontWeight: FontWeight.bold,
                           color: Colors.black,
                         ),
@@ -125,70 +233,6 @@ class _CodePageState extends State<CodePage> {
                   ),
                 );
               },
-            ),
-            StreamBuilder<Duration>(
-              stream: jplayer.positionStream,
-              builder: (context, snapshot) {
-                final position = snapshot.data ?? Duration.zero;
-                return Slider(
-                  onChanged: (newValue) {
-                    jplayer.seek(Duration(milliseconds: newValue.toInt()));
-                  },
-                  value: position.inMilliseconds.toDouble(),
-                  max: jplayer.duration?.inMilliseconds.toDouble() ?? 100.0,
-                );
-              },
-            ),
-            Padding(
-              padding: const EdgeInsets.all(30.0),
-              child: Column(
-                children: [
-                  FloatingActionButton(
-                    onPressed: () async {
-                      if (!isPlaying) {
-                        // Check if the URL has been set already
-                        if (!isUrlSet) {
-                          // Fetch the audioUrl from Firestore
-                          DocumentSnapshot gameSession = await FirebaseFirestore
-                              .instance
-                              .collection('game sessions')
-                              .doc(gameCode)
-                              .get();
-
-                          String? audioUrl = (gameSession.data()
-                              as Map<String, dynamic>)['audio url'];
-
-                          if (audioUrl != null && audioUrl.isNotEmpty) {
-                            await jplayer.setUrl(audioUrl);
-                            isUrlSet =
-                                true; // Set the flag to true after setting the URL
-                          } else {
-                            print("Audio URL not found or is empty");
-                            return;
-                          }
-                        }
-                        jplayer.play();
-                      } else {
-                        jplayer.pause();
-                      }
-                      setState(() {
-                        isPlaying = !isPlaying;
-                      });
-                    },
-                    backgroundColor: Colors.black,
-                    child: Icon(isPlaying ? Icons.pause : Icons.play_arrow),
-                  ),
-                  const SizedBox(height: 10),
-                  const Text(
-                    "Play",
-                    style: TextStyle(
-                      fontSize: 18,
-                      fontWeight: FontWeight.bold,
-                      color: Colors.black,
-                    ),
-                  ),
-                ],
-              ),
             ),
           ],
         ),
--- lib/screen/new.dart
+++ lib/screen/new.dart
@@ -1,138 +1,73 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:flow_app/providers/game_code.dart';
-import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
-import 'package:just_audio/just_audio.dart';
+// // ... (rest of your imports)
 
-class CodePage extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    // Get the game code from the provider
-    final gameCode = Provider.of<GameCodeProvider>(context).code;
-    final AudioPlayer jplayer = AudioPlayer(); // Create a player
+// class CodePage extends StatefulWidget {
+//   @override
+//   _CodePageState createState() => _CodePageState();
+// }
 
-    return Scaffold(
-      body: Container(
-        decoration: const BoxDecoration(
-          gradient: LinearGradient(
-            begin: Alignment.topLeft,
-            end: Alignment.bottomRight,
-            colors: [Color(0xFF83a4d4), Color(0xFFb6fbff)],
-          ),
-        ),
-        child: Column(
-          children: [
-            Padding(
-              padding: const EdgeInsets.only(top: 120.0, bottom: 30.0),
-              child: Text(
-                "Game Code: $gameCode",
-                style: TextStyle(
-                  fontSize: 28,
-                  fontWeight: FontWeight.bold,
-                  color: Colors.black,
-                ),
-              ),
-            ),
-            Expanded(
-              child: StreamBuilder<QuerySnapshot>(
-                stream: FirebaseFirestore.instance
-                    .collection('game sessions')
-                    .doc(gameCode)
-                    .collection('players')
-                    .snapshots(),
-                builder: (context, snapshot) {
-                  if (snapshot.connectionState == ConnectionState.waiting) {
-                    return const Center(child: CircularProgressIndicator());
-                  }
-                  if (!snapshot.hasData || snapshot.data!.docs.isEmpty) {
-                    return const Center(child: Text('No players found.'));
-                  }
-                  final players = snapshot.data!.docs;
+// class _CodePageState extends State<CodePage> {
+//   // ... (rest of your class variables)
 
-                  return ListView.builder(
-                    padding: const EdgeInsets.symmetric(
-                        horizontal: 20.0, vertical: 10.0),
-                    itemCount: players.length,
-                    itemBuilder: (context, index) {
-                      final playerName = players[index].id;
-                      return Padding(
-                        padding: const EdgeInsets.symmetric(vertical: 8.0),
-                        child: Container(
-                          padding: const EdgeInsets.symmetric(
-                              horizontal: 20, vertical: 10),
-                          decoration: BoxDecoration(
-                            color: Colors.white.withOpacity(0.7),
-                            borderRadius: BorderRadius.circular(10),
-                            boxShadow: [
-                              BoxShadow(
-                                color: Colors.black.withOpacity(0.1),
-                                spreadRadius: 1,
-                                blurRadius: 3,
-                                offset: Offset(0, 2),
-                              ),
-                            ],
-                          ),
-                          child: Text(
-                            playerName,
-                            style: TextStyle(
-                              fontSize: 20,
-                              fontWeight: FontWeight.bold,
-                              color: Colors.black,
-                            ),
-                          ),
-                        ),
-                      );
-                    },
-                  );
-                },
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.all(30.0),
-              child: Column(
-                children: [
-                  FloatingActionButton(
-                    onPressed: () async {
-                      // Get the game code from the provider
-                      final gameCode =
-                          Provider.of<GameCodeProvider>(context, listen: false)
-                              .code;
+//   @override
+//   Widget build(BuildContext context) {
+//     // ... (rest of your build method)
 
-                      // Fetch the audioUrl from Firestore
-                      DocumentSnapshot gameSession = await FirebaseFirestore
-                          .instance
-                          .collection('game sessions')
-                          .doc(gameCode)
-                          .get();
+//     return Scaffold(
+//       // ... (rest of your Scaffold)
 
-                      String? audioUrl = (gameSession.data()
-                          as Map<String, dynamic>)['audio url'];
+//       child: Column(
+//         children: [
+//           // ... (rest of your Column children)
 
-                      if (audioUrl != null && audioUrl.isNotEmpty) {
-                        final duration = await jplayer.setUrl(audioUrl);
-                        jplayer.play();
-                      } else {
-                        print("Audio URL not found or is empty");
-                      }
-                    },
-                    child: Icon(Icons.play_arrow),
-                    backgroundColor: Colors.black,
-                  ),
-                  SizedBox(height: 10),
-                  Text(
-                    "Play",
-                    style: TextStyle(
-                      fontSize: 18,
-                      fontWeight: FontWeight.bold,
-                      color: Colors.black,
-                    ),
-                  ),
-                ],
-              ),
-            ),
-          ],
-        ),
-      ),
-    );
-  }
-}
+//           StreamBuilder<Duration>(
+//             stream: jplayer.positionStream,
+//             builder: (context, positionSnapshot) {
+//               final position = positionSnapshot.data ?? Duration.zero;
+//               return Padding(
+//                 padding: const EdgeInsets.symmetric(horizontal: 20.0),
+//                 child: Row(
+//                   mainAxisAlignment: MainAxisAlignment.spaceBetween,
+//                   children: [
+//                     Text(
+//                       "${position.inMinutes}:${position.inSeconds.remainder(60).toString().padLeft(2, '0')}", // Formatting the position to MM:SS format
+//                       style: const TextStyle(
+//                         fontSize: 16,
+//                         fontWeight: FontWeight.bold,
+//                         color: Colors.black,
+//                       ),
+//                     ),
+//                     Expanded(
+//                       child: Slider(
+//                         onChanged: (newValue) {
+//                           jplayer.seek(Duration(milliseconds: newValue.toInt()));
+//                         },
+//                         value: position.inMilliseconds.toDouble(),
+//                         max: jplayer.duration?.inMilliseconds.toDouble() ?? 100.0,
+//                       ),
+//                     ),
+//                     StreamBuilder<Duration>(
+//                       stream: jplayer.durationStream.map((event) => event ?? Duration.zero),
+//                       builder: (context, durationSnapshot) {
+//                         final duration = durationSnapshot.data ?? Duration.zero;
+//                         return Text(
+//                           "${duration.inMinutes}:${duration.inSeconds.remainder(60).toString().padLeft(2, '0')}", // Formatting the duration to MM:SS format
+//                           style: const TextStyle(
+//                             fontSize: 16,
+//                             fontWeight: FontWeight.bold,
+//                             color: Colors.black,
+//                           ),
+//                         );
+//                       },
+//                     ),
+//                   ],
+//                 ),
+//               );
+//             },
+//           ),
+
+//           // ... (rest of your Column children)
+//         ],
+//       ),
+//     );
+//   }
+// }
